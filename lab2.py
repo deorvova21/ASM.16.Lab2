1696b8a356a6d4c6e071a5e5604671e19519b3e6
dl <dl@bugtraq.ru>
2016-09-13 01:03:29 +0300

    (*)

--- lab2.py
+++ lab2.py
@@ -3,18 +3,16 @@ from http.server import HTTPServer, CGIHTTPRequestHandler
 
 port = 80
 webdir = '.'
+if len(sys.argv) > 1:
+	webdir = sys.argv[1]
+if len(sys.argv) > 2:
+	port = int(sys.argv[2])
 
 def launcher():
 	time.sleep(3)
 	os.system("start http://localhost:{0}/cgi-bin/lab2.py".format(port))
 
 def main():
-	global webdir, port
-	if len(sys.argv) > 1:
-		webdir = sys.argv[1]
-	if len(sys.argv) > 2:
-		port = int(sys.argv[2])
-
 	print('webdir "%s", port %s' % (webdir, port))
 	os.chdir(webdir)
 
